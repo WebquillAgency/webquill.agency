8c288bac07eafcc2a9ced2c805c554e1dc5a8a01
WebquillAgency <webquill.agency@gmail.com>
2025-08-31 00:26:21 +0530

    Delete quantum-forge directory

--- quantum-forge/client/pages/shared/Section.tsx
+++
@@ -1,6 +0,0 @@
-import { HTMLAttributes } from "react";
-import { cn } from "@/lib/utils";
-
-export default function Section({ className, ...props }: HTMLAttributes<HTMLElement>) {
-  return <section {...props} className={cn("relative overflow-hidden", className)} />;
-}
